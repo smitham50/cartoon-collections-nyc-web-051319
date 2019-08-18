89be7b51d77cfb9a71a96a9f49aaed35cb41a61d
Aaron Smith <smitham50@gmail.com>
2019-08-18 14:37:58 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,22 +1,21 @@
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |dwarf, index|
-    puts "#{index+1}. #{dwarf}"
+    puts "#{index + 1} #{dwarf}"
   end
 end
 
 def summon_captain_planet(calls)
-  calls.collect do |call|
+  calls.map do |call|
     "#{call.capitalize}!"
   end
 end
 
-def long_planeteer_calls(words)
-  words.any? {|word| word.length > 4}
+def long_planeteer_calls(calls)
+  calls.any? {|call| call.length > 4}
 end
 
 def find_the_cheese(foods)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  foods.find do |food|
-    cheese_types.include?(food)
-  end
+  cheeses = ["cheddar", "gouda", "camembert"]
+
+  foods.find {|food| cheeses.include?(food)}
 end
